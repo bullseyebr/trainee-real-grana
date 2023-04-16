76a25a5558d3b29bdeabc670222be77e907c9270
viniman <vinicius.carlos@live.com>
2023-04-16 00:50:37 -0300

    create component of card and include on index.php

---
+++ components/card.php
@@ -0,0 +1,10 @@
+<div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
+    <img class="card-img-top" src="https://picsum.photos/700/300?v=<?= $i ?>" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+            of
+            the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -139,16 +139,7 @@ $array = array("Volvo", "BMW", "Toyota");
         <section>
             <div class="row text-center">
                 <?php for($i = 0; $i < 4; $i++) : ?>
-                    <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
-                        <img class="card-img-top" src="https://picsum.photos/700/300?v=<?= $i ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+                    <?php include 'components/card.php'; ?>
                 <?php endfor; ?>
             </div>
             <div>
