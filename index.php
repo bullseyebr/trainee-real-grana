3a7d75eacc5acc4af0ae58606e26f671fa59de94
viniman <vinicius.carlos@live.com>
2023-04-16 00:43:24 -0300

    optimize creation of cards with php

--- index.php
+++ index.php
@@ -138,47 +138,18 @@ $array = array("Volvo", "BMW", "Toyota");
         </section>
         <section>
             <div class="row text-center">
-
-                <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
-                    <img class="card-img-top" src="https://picsum.photos/700/300?v=14" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <?php for($i = 0; $i < 4; $i++) : ?>
+                    <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
+                        <img class="card-img-top" src="https://picsum.photos/700/300?v=<?= $i ?>" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                </div>
-                <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
-                    <img class="card-img-top" src="https://picsum.photos/700/300?v=18" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
-                    <img class="card-img-top" src="https://picsum.photos/700/300?v=71" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="card col-lg-4 col-md-6 col-12 m-2 shadow-lg" style="width: 18rem;">
-                    <img class="card-img-top" src="https://picsum.photos/700/300?v=6" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+                <?php endfor; ?>
             </div>
             <div>
                 <nav class="shadow-lg" aria-label="Page navigation example">
