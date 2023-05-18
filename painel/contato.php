3cb7fafd8fb980d6a79c695cbe74ec7d51c4eb47
viniman <vinicius.carlos@live.com>
2023-05-18 18:35:47 -0300

    create components of head and  header, and change and include them in pages

--- painel/contato.php
+++ painel/contato.php
@@ -29,22 +29,12 @@ $array = array("Volvo", "BMW", "Toyota");
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <!-- Meta tags Obrigatórias -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <title>Home</title>
-</head>
+<?php include('../include/head.php'); ?>
 
 <body id="main-container">
-    <?php include('../include/navbar.php'); ?>
+    <?php include('../include/header.php'); ?>
     <main class="container">
-
+        <h1>Contato</h1>
     </main>
 
     <?php include('../include/footer.php'); ?>
